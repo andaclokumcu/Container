8f57d1d0c8c12471f498dd28477b7cfc180cf2a5
Andaç Lokumcu <andaclokumcu@hotmail.com>
2021-02-23 21:34:39 +0300

    Class Example

---
+++ Class/Kurs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class
+{
+    //Classların isimlerinin ilk harfi büyük olur.
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string Egitmen { get; set; } 
+        public int IzlenmeOrani { get; set; } 
+    }
+}
---
+++ Class/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kurs kurs1 = new Kurs(); //Kurs sınıfından ilk verileri çağırdık.
+            kurs1.KursAdi = "C#";
+            kurs1.Egitmen = "Engin Demiroğ";
+            kurs1.IzlenmeOrani = 68;
+
+            Console.WriteLine(kurs1.KursAdi + ":" + kurs1.Egitmen + ":" + kurs1.IzlenmeOrani);
+
+            Kurs[] kurslar = new Kurs[] { kurs1 }; 
+  //Başka kursları, eğitmenlerini ve izlenme oranını Kurs sınıfı içindeki verilerden kurs2,kurs3 olarak tanımlayıp devam edebiliriz.
+             
+        }
+    }
+}
