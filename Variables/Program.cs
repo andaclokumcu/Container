c9984c075939f6002bd61091dd66006b9bc7fa1d
Andaç Lokumcu <andaclokumcu@hotmail.com>
2021-02-23 17:53:42 +0300

    Add project files.

---
+++ Variables/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string kategoriEtiketi = "Kategoriler"; --> Genellikle metinsel değerleri string içinde tanımlarız.
+            // int ogrencisayisi = 32000; --> Tam sayı değerlerini böyle tanımlarız.
+            // double faizoranı = 1,45; --> Çift duyarlı sayıları tanımlarken
+            // bool sistemeGirisYapmisMi = false --> True ve false olarak değer alır. Genellikle kontrol işlmlerinde kullanılır.
+
+            bool sistemeGirisYapmisMi = false;
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giris Yap Butonu");
+            }
+
+        }
+    }
+}
