98429036aecc744fb67aa0b5b85b6decca520804
Andaç Lokumcu <andaclokumcu@hotmail.com>
2021-02-23 20:57:44 +0300

    For Loop Example

---
+++ Loop (for)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Loop__for_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            // i = 1'den başla, i 10'dan küçük kalana kadar 10'a kadar arttırarak devam et.
+        }
+    }
+}
