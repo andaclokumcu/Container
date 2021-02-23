d292a734079639393fc5be78301e540c2b18e7cd
Andaç Lokumcu <andaclokumcu@hotmail.com>
2021-02-23 17:56:18 +0300

    Dolar Example

---
+++ Dolar Example/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Dolar_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalıs");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artis");
+            }
+            else
+            {
+                Console.WriteLine("Degismedi");
+
+            }
+        }
+    }
+}
