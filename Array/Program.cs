bfb13f09b38df026a7e181f589271207354eb103
Andaç Lokumcu <andaclokumcu@hotmail.com>
2021-02-23 21:14:35 +0300

    Course Example

---
+++ Array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "C#";
+            string kurs2 = "Python"; //C#,Python ve Java olarak 3 kurs tanımladık.
+            string kurs3 = "Java";
+
+            string[] kurslar = new string[] { "C#", "Python", "Java" };  //kurslar adında bütün kursları barındıran tanım yapıldı.
+
+            for (int i = 0; i < kurslar.Length; i++) //0.elemandan başlayarak kursları uzunluğunca ekrana ver.
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            foreach (string kurs in kurslar) //dizi temelli yapıları tek tek dönmeye yarar yani kursları tek tek dolaşır.
+            {
+                Console.WriteLine(kurs);
+            }
+
+        }
+    }
+}
