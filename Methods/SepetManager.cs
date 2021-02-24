f79876c3e4d3fb9f9f6ea0a4bd75286dc5952ce4
Andaç Lokumcu <andaclokumcu@hotmail.com>
2021-02-24 17:25:30 +0300

    Product Example

---
+++ Methods/SepetManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    class SepetManager
+    {
+        public void Ekle(Urun urun)
+        {
+        Console.WriteLine("Tebrikler, ürün sepete eklendi : " + urun.Adi);
+        }
+
+        public void Ekle2(string urunAdi, string Aciklama, double Fiyati)
+        {
+        Console.WriteLine("Tebrikler, ürün sepete eklendi : " + urunAdi);
+        }
+
+  }
+}
