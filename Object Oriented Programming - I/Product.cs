93c010104bae991a600fd0add190918f56a336f2
Andaç Lokumcu <andaclokumcu@hotmail.com>
2021-02-24 21:32:49 +0300

    e-Commerce Site Example

---
+++ Object Oriented Programming - I/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Object_Oriented_Programming___I
+{
+    class Product
+    {
+        //ürün tabanlı bir e-ticaret sitesi
+
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+
+    }
+}
---
+++ Object Oriented Programming - I/ProductManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Object_Oriented_Programming___I
+{
+    class ProductManager
+    {
+        //void ile git ekle, git günclle gibi emirleri vererek işlem yaptırıyoruz.
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName +" "+ "eklendi.");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + "güncellendi.");
+        }
+        
+
+    }
+}
