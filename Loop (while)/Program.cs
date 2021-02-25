15c6cea6ae0928d52977759df78589539b9bfdf8
Andaç Lokumcu <andaclokumcu@hotmail.com>
2021-02-25 23:11:05 +0300

    "Hello" Example

---
+++ Loop (while)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Loop__while_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Belirlenen komut bloğunu koşul sağlandığı sürece çalıştıran temel döngü yapılarından bir tanesidir. 
+            //Döngü koşul deyimi true olduğu sürece çalıştırılır, koşul false olduğunda ise döngüden çıkılır.
+            //While döngüsünde döngü değişkeni bulunmaz, bunun yerine while bloğuna girmeden başlangıçta değeri bilinen bir değişken kullanılır.
+
+            int i = 1;
+
+            while (i<=10)
+            {
+                Console.WriteLine("Hello");
+                i++;
+            }
+
+        }
+    }
+}
