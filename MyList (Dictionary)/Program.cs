f64f8f19a1ae8ab5ee5ac8e1cc8b854fa1ecb818
Andaç Lokumcu <andaclokumcu@hotmail.com>
2021-02-26 18:53:52 +0300

    Dictionary Example

---
+++ MyList (Dictionary)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyList__Dictionary_
+{
+    class Program
+    {
+       
+            static Dictionary<string, int> kutuphane = new Dictionary<string, int>();
+
+
+            static void Main(string[] args)
+            {
+                kutuphane.Add("Andaç", 25);
+                kutuphane.Add("Ayhan", 55);
+                kutuphane.Add("Özlem", 49);
+                kutuphane.Add("İpek", 20);
+
+                foreach (var item in kutuphane)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+    }
+}
